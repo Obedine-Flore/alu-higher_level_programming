a128a81d4b25cf02fc7d1a64c9cf3b6c1b471e14
Obedine-Flore <f.obedine@alustudent.com>
2022-10-23 10:32:45 -0700

    my commit

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,7 +4,7 @@ number = random.radiant(-10,10)
 for n in number:
     if n > 0:
         print(n, "is positive")
-    elif n = 0:
+    elif n == 0:
         print(n, "is zero")
     elif n < 0:
         print(n, "is negative")

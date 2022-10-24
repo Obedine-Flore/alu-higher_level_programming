2e4fe73759bdf066bc6ff45d0f324beb743e8024
Obedine-Flore <f.obedine@alustudent.com>
2022-10-24 07:58:17 -0700

    my commit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,4 @@ if last_dig == 0:
 elif last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
 else:
-    print(
-    f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,2 @@
+for i in range(91,122):
+    print(i, i++)

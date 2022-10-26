397fa4329d4b74c6a01cc02e2c8e1c4dd41b3ff1
Obedine-Flore <f.obedine@alustudent.com>
2022-10-26 12:59:40 -0700

    my commit

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-result_str = ""
-s = True
-for c in range(ord('a'), ord('b')):
-    result_str += i.upper() if s else i.lower()
-    if i.isalpha():
-       s = not s
-return result_str
-print("{:c}".format(c), end="")
+for c in range(ord('z'), ord('a')):
+    print('{:c}{:s}".format(c, chr(c-33)), end="")

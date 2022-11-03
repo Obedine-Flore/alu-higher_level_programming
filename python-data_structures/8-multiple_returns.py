7d77ac1e20545e5e2f3bed478423639e3274ac20
Obedine-Flore <f.obedine@alustudent.com>
2022-11-03 04:41:00 -0700

    my commit

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first_character = None
+    else:
+        first_character = sentence[0]
+
+        return(length, first_character)

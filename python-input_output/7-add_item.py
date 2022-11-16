70411807d9510792faf8ae8026dc40a87245272a
Obedine-Flore <f.obedine@alustudent.com>
2022-11-16 13:48:23 -0800

    my commit

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''adds all arguments to a python list and then saves them to a file'''
+
+
+import sys
+
+if _name_ == "_main_":
+    save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
+    load_from_json_file = _import_('6-load_form_json_file')\
+            .load_from_json_file
+
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''the dictionary description with
+simple data structures (list, dictionaries,
+strings, integers, and boolean)
+for JSON serialization of an object.
+'''
+
+
+def class_to_json(obj):
+    '''Creates a dict description of obj.
+    Args:
+        - obj: object to serialize
+    Returns: dictionary description of obj
+    '''
+
+
+    return obj._dict_

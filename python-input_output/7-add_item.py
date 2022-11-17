b328bcfc42ce66e6b8f44d44a523e1f92895c219
Obedine-Flore <f.obedine@alustudent.com>
2022-11-17 10:49:16 -0800

    my commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''adds all arguments to a python list and then saves them to a file:'''
+'''adds all arguments to a python list, and then save them to a file'''
 
 
 import sys
 
 if _name_ == "_main_":
     save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-    load_from_json_file = _import_('6-load_from_json_file')\
-        .load_from_json_file
+    load_from_json_file = _import_('6-load_from_json_file') \
+            .load_from_json_file
 
     try:
         loadFile = load_from_json_file("add_item.json")

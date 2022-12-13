b506111bd510706f15a3ed7606d62ce366f54db3
Obedine-Flore <f.obedine@alustudent.com>
2022-12-13 06:32:52 -0800

    my commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,11 +12,11 @@ import sys
 if _name_ == "_main_":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3])
-    crs = db.cursor()
-    crs.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    query_rows = crs.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    query_rows = cur.fetchall()
     for row in querry_rows:
         if row[1][0] == 'N':
             print(row)
-        crs.close()
+        cur.close()
         db.close()

54b351f7a1a8c04aa4981179e244a12cc0386be5
Obedine-Flore <f.obedine@alustudent.com>
2022-12-13 07:08:42 -0800

    my commit

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,18 +8,13 @@ from sys import argv
 
 
 if __name__ == "_main_":
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     crs = db.cursor()
     crs.execute("SELECT * FROM staes WHERE name LIKE %S ORDER BY id ASC",
                 (argv[4],))
     rows = crs.fetchall()
-    for row in rows:
-        print(row)
+    for i in rows:
+        print(i)
     crs.close()
     db.close()

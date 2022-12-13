2b8e00308a7b18cc5a71bbdd00b10b0fc78fc018
Obedine-Flore <f.obedine@alustudent.com>
2022-12-13 12:39:39 -0800

    my commit

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,25 +10,37 @@ max_integer = _import_('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_ordered_list(self):
+        '''case of an ordered list'''
         self.assertEqual(max_integer([2, 4, 6, 8]), 8)
-        ''' case of an ordered list'''
 
     def test_unordered_list(self):
+        '''case of an unordered list'''
         self.assertEqual(max_integer([6, 2, 8, 4]), 8)
-        ''' case of an unordered list '''
 
     def test_single_element(self):
+        '''case of a single element'''
         self.assertEqual(max_integer([1]), 1)
-        ''' case of a single element'''
 
     def test_floats(self):
+        '''case with floating point numbers'''
         self.assertEqual(max_integer([1.0, 2.2, 3.5, 4.8]), 4.8)
-        ''' case with decimal numbers'''
 
     def test_string(self):
+        '''case with a string'''
         self.assertEqual(max_integer(["Baby"]), 'y')
-        ''' case with a string'''
 
     def test_empty_string(self):
+        '''case of an empty list'''
         self.assertEqual(max_integer(""), None)
-        ''' case with an empty list'''
+
+    def test_max_at_begining(self):
+        self.asserEqual(max_integer([8, 7, 6, 4]), 8)
+        ''' case with max at the begining'''
+
+    def test_max_at_end(self):
+        ''' case with max at the end'''
+        self.assertEqual([2, 5, 4, 7]), 7)
+
+    def test_integers(self):
+        '''case with negative numbers'''
+        self.assertEqual([-1, 4, -5, 3]), 4)

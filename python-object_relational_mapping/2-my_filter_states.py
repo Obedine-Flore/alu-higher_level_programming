aad217e8ac91a6ec353744619be92b45884020e6
Obedine-Flore <f.obedine@alustudent.com>
2022-12-13 06:44:39 -0800

    my commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""This script takes an argument and displays the argument"""
+"""Script that takes in an argument and displays all values in the states"""
 
 import MySQLdb
 import sys
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     crs = db.cursor()
-    crs.execute("SELECT * FROM staes WHERE name LIKE BINARY '{}'\
-            ORDER BY id ASC".FORMAT(SYS.ARGV[4]))
+    crs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY id ASC".format(sys.argv[4]))
     rows = crs.fetchall()
     for row in rows:
         if row[1] == sys.argv[4]:

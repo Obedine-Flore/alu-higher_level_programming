9b1a2d0df89ee7aa583d16998918c317c9e15d1d
Obedine-Flore <f.obedine@alustudent.com>
2022-12-14 11:54:45 -0800

    my commit

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-This script matches values in staes table of hbtn_0e
-"""
+"""This script that takes in an argument and displays all values in the states"""
 
 import MySQLdb
 from sys import argv
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     crs = db.cursor()
-    crs.execute("SELECT * FROM staes WHERE name LIKE %S ORDER BY id ASC",
+    crs.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
                 (argv[4],))
     rows = crs.fetchall()
     for i in rows:

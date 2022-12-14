db11be904c1dd2af2cc90775292914a2346e7b47
Obedine-Flore <f.obedine@alustudent.com>
2022-12-14 12:15:58 -0800

    my commit

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,54 +1,52 @@
 #!/usr/bin/python3
-"""
-This function performs a unit test for the max. integer
-"""
+"""Unittest for max_integer([..])"""
 
 import unittest
-max_integer = _import_('6-max_integer').max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer"""
 
     def test_ordered_list(self):
-        '''case of an ordered list'''
+        """case of an ordered list"""
         self.assertEqual(max_integer([2, 4, 6, 8]), 8)
 
     def test_unordered_list(self):
-        '''case of an unordered list'''
-        self.assertEqual(max_integer([6, 2, 8, 4]), 8)
-
-    def test_single_element(self):
-        '''case of a single element'''
-        self.assertEqual(max_integer([1]), 1)
-
-    def test_floats(self):
-        '''case with floating point numbers'''
-        self.assertEqual(max_integer([1.0, 2.2, 3.5, 4.8]), 4.8)
-
-    def test_string(self):
-        '''case with a string'''
-        self.assertEqual(max_integer(["Baby"]), 'y')
-
-    def test_empty_string(self):
-        '''case of an empty list'''
-        self.assertEqual(max_integer(""), None)
-
-    def test_max_at_begining(self):
-        self.asserEqual(max_integer([8, 7, 6, 4]), 8)
-        ''' case with max at the begining'''
-
-    def test_max_at_end(self):
-        ''' case with max at the end'''
-        self.assertEqual([2, 5, 4, 7]), 7)
-
-    def test_integers(self):
-        '''case with negative numbers'''
-        self.assertEqual([-1, 4, 1, 3]), 4)
-
-    def test_negative_integers(self):
-        '''case with negative integers only'''
-        self.assertEqual([-1, -2, -3, -4]), -1)
+        """case of an unordered list"""
+        self.assertEqual(max_integer([5, 7, 1, 6]), 6)
 
     def test_empty_list(self):
-        '''case with an empty list'''
-        self.assertEqual([], None)
+        """case of an empty list"""
+        self.assertEqual(max_integer([]), None)
+    
+    def test_max_at_beginning(self):
+        """case with max at the begining"""
+        self.assertEqual(max_integer([8, 6, 4, 2]), 8)
+
+    def test_one_element(self):
+        """case with a single element"""
+        self.assertEqual(max_integer([2]), 2)
+
+    def test_floats(self):
+        """case with floats"""
+        self.assertEqual(max_integer([1.0, 2.0, 3.0, -4.0, 5.0]), 5.0)
+
+    def test_ints_and_floats(self):
+        """case with integers and floats"""
+        self.assertEqual(max_integer([1.0, 3.5, -2, 4, 5.5]), 5.5)
+
+    def test_string(self):
+        """case of a string"""
+        self.assertEqual(max_integer("School"), 'o')
+
+    def test_list_of_strings(self):
+        """case with a list of strings"""
+        self.assertEqual(max_integer(["I", "Am", "Happy"]), "Happy")
+
+    def test_empty_string(self):
+        """case of an empty string"""
+        self.assertEqual(max_integer(""), None)
+    
+if __name__ == '__main__':
+    unittest.main()

0ebcf02a59848d3c9c6f055b6c6f1564695f9b21
Obedine-Flore <f.obedine@alustudent.com>
2022-12-14 05:56:00 -0800

    my commit

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -10,8 +10,8 @@ from sys import argv
 if __name__ == "_main_":
     # Create engine
     engine = cretae_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(argv[1], argv[2], argv[3]),
-                            pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     # Create session
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)

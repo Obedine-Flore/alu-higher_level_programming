9c0d05d9a6521d1a5d23129425f102936816e6c6
Obedine-Flore <f.obedine@alustudent.com>
2022-12-15 04:42:33 -0800

    my commit

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-"""
-This function divides each element of a matrix
-"""
+"""This script efines a matrix division function."""
+
 
 def matrix_divided(matrix, div):
-    """divides matrix by constant"""
+    """This function divides all elements of a matrix."""
     if not isinstance(matrix, (list,)):
-        raise TypeError("matrix must be a matrix
-                        (list of lists) of integers or floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     for row in matrix:
         if type(row) != list:
-            raise TypeError("matrix must be a matrix
-                            (list of lists) of integers or floats")
-        for element in row:
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
+        for item in row:
             if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix
-                                (list of lists) of integersor floats")
+                raise TypeError("matrix must be a matrix"
+                                " (list of lists) of integers/floats")
     size = len(matrix[0])
     for row in matrix:
         if len(row) != size:
             raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
-        raise TypeError("div must be number")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    matrix = []
+    mat = []
     for i in range(len(matrix)):
-        matrix.append(list())
+        mat.append(list())
         for j in range(len(matrix[i])):
-            matrix[i].append(round(matrix[i][j] / div, 2))
-    return matrix
+            mat[i].append(round(matrix[i][j] / div, 2))
+    return mat

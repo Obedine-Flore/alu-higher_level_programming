478d754406ed18d8e3cc7eb829a31c6b627cda73
Obedine-Flore <f.obedine@alustudent.com>
2023-02-10 19:48:29 -0800

    my commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
-#!/usr/bin/node
-module.exports = class Rectangle {
+##!/usr/bin/node
+class Rectangle {
 	constructor (w, h) {
 		if (w > 0 && h > 0) {
 			this.width = w;
 			this.height = h;
 		}
 	}
-};
+}
+module.exports = Rectangle;
--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
 	constructor (w, h) {
-		if (w > 0 && h > 0) {
+		if (w > 0 && h >0) {
 			this.width = w;
 			this.height = h;
 		}
 	}
 
 	print () {
-		for (let i = 0; i < this height; i++) {
+		for (let i = 0; i < this.height; i++) {
 			console.log('X'.repeat(this.width));
 		}
 	}
-};
+}
+module.exports = Rectangle;
+

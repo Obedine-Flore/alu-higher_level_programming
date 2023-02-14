fd176bc2abb38bd701fc67bed5c1a6475b4eeac8
Obedine-Flore <f.obedine@alustudent.com>
2023-02-14 15:31:29 -0800

    my commit

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
-function factorise (num) {
-	let factorial = 1;
-	for (let i = 1; i <= num; i++) {
-		factorial *= i;
-	}
-	return factorial;
-}
-
-console.log(factorise(num));
+function factorise (num) { let factorial = 1;
+	for (let i = 1; i <= num; i++) { factorial *= i; }
+	return factorial; } console.log(factorise(num));
--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const myArray = process.argv;
-if (myArray.length <= 3) { console.log(0);
-} else {
-	console.log(myArray.sort((a, b) => b - a).slice(3)[0]); }
+if (myArray.length <= 3) {
+	console.log(0); } else { console.log(myArray.sort((a, b) => b - a).slice(3)[0]); }

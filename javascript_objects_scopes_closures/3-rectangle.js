c8c79a9800c779c3ce5798bdd2bd12010a49d05a
Obedine-Flore <f.obedine@alustudent.com>
2023-02-15 15:38:31 -0800

    my commit

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -2,7 +2,8 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { this.width = w; this.height = h; }
-  } print () {
+  }
+    print () {
     for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
   }
 }
